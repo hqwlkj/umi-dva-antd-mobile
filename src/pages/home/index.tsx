e24a35ba90f3bb1826c7b653d5b904791b8464ea
yanghc <hqwlkj@outlook.com>
2019-02-02 09:56:36 +0800

    fix Failed to compile

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,34 +1,34 @@
+import { Button } from 'antd-mobile';
 import React from 'react';
-import {Button} from 'antd-mobile';
 import router from 'umi/router';
 
 const styles = require('./index.less');
 
 
-export default class extends React.Component<{}> {
+export default class extends React.Component<{}, {}, any> {
   constructor(props) {
     super(props);
   }
 
   public render = () => {
     return (<div className={styles.home}>
-    
-      <div
-          className={styles.index}
-        >
-          <div className={styles.title}>网约车从业资格证</div>
-          <div className={styles.subtitle}>考题练习及模拟考试</div>
-          <Button
-            type='primary'
-            className={styles['btn-enter']}
-            onClick={() => {
-              // this.setState({ step: 1 });
-              router.push('/entrance');
 
-            }}
-          >进入
-          </Button>
-        </div>
+      <div
+        className={styles.index}
+      >
+        <div className={styles.title}>网约车从业资格证</div>
+        <div className={styles.subtitle}>考题练习及模拟考试</div>
+        <Button
+          type='primary'
+          className={styles['btn-enter']}
+          onClick={() => {
+            // this.setState({ step: 1 });
+            router.push('/entrance');
+
+          }}
+        >进入
+        </Button>
+      </div>
     </div>);
   };
 }
