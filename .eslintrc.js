c89c07ebc48464f6ffb071aadeff7f548de27b64
yanghc <hqwlkj@outlook.com>
2019-10-22 11:52:56 +0800

    fix #31

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'plugin:compat/recommended'],
+  plugins: [
+    'react-hooks',
+  ],
   env: {
     browser: true,
     node: true,
@@ -21,7 +24,9 @@ module.exports = {
     'react/jsx-tag-spacing': 0,
     'react/forbid-prop-types': 0,
     'react/jsx-one-expression-per-line': 0,
-    'import/no-unresolved': [2, { ignore: ['^@/', '^umi/'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/no-unresolved': [2, { ignore: ['^@/', '^umi/', '^antd', '^./'] }],
     'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': [
       2,
@@ -30,15 +35,18 @@ module.exports = {
         devDependencies: ['**/tests/**.js', '/mock/**.js', '**/**.test.js'],
       },
     ],
+    'import/no-cycle': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': 0,
     'linebreak-style': 0,
-    quotes: ['error', 'single'], // 使用单引号
-    semi: ['error', 'always'], // 结束添加分号
+    'quotes': ['error', 'single'], // 使用单引号
+    'semi': ['error', 'always'], // 结束添加分号
   },
   settings: {
-    polyfills: ['fetch', 'promises', 'url'],
+    // support import modules from TypeScript files in JavaScript files
+    'import/resolver': { node: { extensions: ['.js', '.ts', '.tsx'] } },
+    polyfills: ['fetch', 'promises', 'url', 'object-assign'],
   },
 };
